a72ecfa1d00875022d3928dc0fa88f79334ca457
nsd21253-ui <nsd21253@gmail.com>
2026-06-13 22:42:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: '/macushla-cricket-hub/'
+    base: '/macushla-cricket-hub'
   }
 });
+
